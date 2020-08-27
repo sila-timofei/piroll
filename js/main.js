39fc2b52f45bb1524770f7a9d1679cd919049aaa
Timofey <silatimofey@yandex.com>
2020-08-27 23:30:25 +0300

    slider rewiews

---
+++ js/main.js
@@ -0,0 +1,44 @@
+window.addEventListener('DOMContentLoaded', function() {
+    const text = document.querySelectorAll('.text-review'),
+          dots = document.querySelectorAll('.pagination-item'),
+          wroteRewiews = document.querySelectorAll('.wrote');
+
+    function hideText() {
+        text.forEach(text => {
+            text.classList.remove('show');
+        });
+        wroteRewiews.forEach(text => {
+            text.classList.remove('show');
+        });
+    }
+
+    function changeActivePagination() {
+        dots.forEach(dot => {
+            dot.classList.remove('pagination-active');
+        });
+    }
+
+    function changeClass(i) {
+        dots[i].classList.add('pagination-active');
+        text[i].classList.add('show');
+        wroteRewiews[i].classList.add('show');
+    }
+
+    dots[0].addEventListener('click', () => {
+        changeActivePagination();
+        hideText();
+        changeClass(0);
+    });
+
+    dots[1].addEventListener('click', () => {
+        changeActivePagination();
+        hideText();
+        changeClass(1);
+    });
+
+    dots[2].addEventListener('click', () => {
+        changeActivePagination();
+        hideText();
+        changeClass(2);
+    });
+});
